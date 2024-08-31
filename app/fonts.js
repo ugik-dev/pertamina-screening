aa27772e9fb6a879f97743ac3251e720cf887f35
Ugik DEV <ugik.dev@gmail.com>
2024-09-01 04:38:59 +0700

    add font google from next

---
+++ app/fonts.js
@@ -0,0 +1,11 @@
+import { Roboto, Nerko_One } from "next/font/google";
+
+export const robot = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const nerko = Nerko_One({
+  subsets: ["latin"],
+  weight: ["400"],
+});
