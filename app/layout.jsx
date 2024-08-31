bc410dad7e62f4d655d6c9c1327e0eedf5dd69f7
Ugik DEV <ugik.dev@gmail.com>
2024-08-31 23:06:46 +0700

    learn 1

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
-export default function layout({ children }) {
+export default function Layout({ children }) {
   return (
     <html lang="en">
       <head>
         <title>Pertamina Screening</title>
       </head>
-      <body>{children}</body>
+      <body>
+        <header>[header]</header>
+        <main>{children}</main>
+        <footer>[footer]</footer>
+      </body>
     </html>
   );
 }
